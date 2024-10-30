6f99928f0ebb4ccfab87ed6c78ddb7259f37c5e2
Beam-K <147979946+Beam-K@users.noreply.github.com>
2024-10-30 23:01:30 +0700

    Add files via upload

---
+++ lerningJS.js
@@ -0,0 +1,10 @@
+ //console.log("привет");
+    //console.info("привет");
+    //console.error("ошибка");
+    //console.warn("привет");
+
+
+    var arr = [];
+    arr = [3,4,5,5.3];
+
+    console.log(arr[2]);
